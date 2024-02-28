41ba9a356000c22d892a56397d65c662fd62c713
cipmar <marius.ropotica@gmail.com>
2024-02-28 21:01:29 +0200

    Retry socket connection. Dropping the message after some retries

--- mqqt_consumer/src/main.rs
+++ mqqt_consumer/src/main.rs
@@ -1,120 +1,125 @@
-// file:///Users/mariusropotica/.rustup/toolchains/stable-aarch64-apple-darwin/share/doc/rust/html/book/ch05-03-method-syntax.html
-
-use std::{env, io::Write, net::TcpStream, process, thread, time::Duration};
-use mqtt::Message;
-
-extern crate paho_mqtt as mqtt;
+use paho_mqtt as mqtt;
+use std::{io::Write, net::TcpStream, process, thread, time::Duration};
+use mqtt::{Client, ConnectOptionsBuilder, Message};
 
 const DEFAULT_BROKER:&str = "tcp://127.0.0.1:1883";
-const DEFAULT_CLIENT:&str = "zigbee2mqtt";
+const DEFAULT_CLIENT:&str = "zigbee2mqqt";
 const DEFAULT_TOPICS:&[&str] = &["zigbee2mqtt/thermometer_bedroom", "zigbee2mqtt/thermometer_living", "zigbee2mqtt/thermometer_garden"];
 
 fn main() {
 
-    let host = env::args().nth(1).unwrap_or_else(|| {
-        DEFAULT_BROKER.to_string()
-    });
-
     let create_opts = mqtt::CreateOptionsBuilder::new()
-        .server_uri(host)
+        .server_uri(DEFAULT_BROKER)
         .client_id(DEFAULT_CLIENT)
         .finalize();
-    
-     let cli = mqtt::Client::new(create_opts).unwrap_or_else(|err| {
-        println!("Error creating the client: {:?}", err);
+
+    let client = mqtt::Client::new(create_opts).unwrap_or_else(|err| {
+        println!("Error creating the client {:?}", err);
         process::exit(1);
-     });
+    });
 
-     let receiver = cli.start_consuming();
+    let receiver = client.start_consuming();
 
-     let lwt = mqtt::MessageBuilder::new()
-        .payload("This is the last will and testament of this clinet.
+    let lwt = mqtt::MessageBuilder::new()
+        .payload("This is the last will and testament if this clinet.
             Broker, please publish this message after I die!")
-        .topic("test")
+        .topic("lwt")
         .finalize();
 
-     let conn_opts = mqtt::ConnectOptionsBuilder::new()
-        .keep_alive_interval(Duration::from_secs(10))
+    let conn_opts = ConnectOptionsBuilder::new()
+        .keep_alive_interval(Duration::from_secs(20))
         .clean_session(false)
         .will_message(lwt)
         .finalize();
 
-    if let Err(e) = cli.connect(conn_opts) {
-        println!("Unable to connect: {:?}", e);
+    if let Err(err) = client.connect(conn_opts) {
+        println!("Unable to connect: {:?}", err);
         process::exit(1);
     }
 
-    subscribe_topics(&cli);
+    subscribe_topics(&client);
 
-    println!("Processing messages ...");
+    println!("Receiving messages...");
 
-    for msg in receiver.iter() {
-        if let Some(msg) = msg {
-            process_message(&msg);
-        } else if !cli.is_connected() {
-            if try_reconnect(&cli) {
+    for msq in receiver.iter() {
+        if let Some(msg) = msq {
+            process_messages(&msg);
+        } else if !client.is_connected() {
+            if try_reconnect_mqtt(&client) {
                 println!("Resubscribe to topics...");
-                subscribe_topics(&cli);
+                subscribe_topics(&client);
             } else {
                 break;
             }
         }
     }
 
-    if cli.is_connected() {
+    if client.is_connected() {
         println!("Disconnecting...");
-        cli.unsubscribe_many(DEFAULT_TOPICS).unwrap();
-        cli.disconnect(None).unwrap();
+        client.unsubscribe_many(DEFAULT_TOPICS).unwrap();
+        client.disconnect(None).unwrap();
     }
 
-    println!("Exiting");
-}
-
-fn try_reconnect(cli: &mqtt::Client) -> bool {
-
-    for i in 1..10 {
-        thread::sleep(Duration::from_millis(10000));
-
-        println!("Connection lost, retrying to connect, try {} ...", i);
-
-        if cli.reconnect().is_ok() {
-            println!("Successfully reconnecred!");
-            return true;
-        }
-    }
-
-    println!("Unable to recconect after multiple attempts");
-    false
+    println!("Exiting...")
 }
 
 fn subscribe_topics(cli: &mqtt::Client) {
 
-    if let Err(e) = cli.subscribe_many(DEFAULT_TOPICS, &[0, 1]) {
-        println!("Error subscribe to topics: {:?}", e);
+    if let Err(err) = cli.subscribe_many(DEFAULT_TOPICS, &[mqtt::QOS_0, mqtt::QOS_1]) {
+        println!("Error subscribing to topics: {:?}", err);
         process::exit(1);
-    }   
+    }
 }
 
-fn process_message(msg: &Message) {
+fn try_reconnect_mqtt(client: &Client) -> bool {
 
-    let topic = msg.topic();
-    let payload = msg.payload_str();
+    let retries = 10;
 
-    println!("Received message, topic: {}, payload: {}", topic, payload);
+    for i in 1..retries {
 
-    write_message_to_socket(msg);
+        thread::sleep(Duration::from_secs(10));
+        println!("Connection lost, retrying to connect, try {:?}", i);
+
+        if client.reconnect().is_ok() {
+            println!("Successfully reconnecred.");
+            return true;
+        }
+    }
+
+    println!("Unable to reconnect after {:?} retries", retries);
+    false
 }
 
-fn write_message_to_socket(msg: &Message) {
+fn process_messages(msg: &Message) {
 
-    let mut tcp_stream = TcpStream::connect("127.0.0.1:34000").unwrap_or_else(|err| {
-        println!("Error connecting to socket: {}", err);
-        process::exit(1);
-    });
+    let retries = 10;
+    let mut connected = false;
 
-    // TODO: encode the message in protobuf format
-    if let Err(e) = tcp_stream.write(msg.payload_str().as_bytes()) {
-        println!("Error writing to the socket: {}", e);
-        process::exit(1);
+    for i in 1..retries {
+
+        match TcpStream::connect("127.0.0.1:34000") {
+            Ok(mut tcp_stream) => {
+                match tcp_stream.write(msg.payload_str().as_bytes()) {
+                    Ok(bytes_written) => {
+                        println!("Message successfully received and sent to socket, {:?} bytes written", bytes_written);
+                        connected = true;
+                        break;
+                    },
+                    Err(e) => {
+                        println!("Error writing to the socket: {:?}", e);
+                        process::exit(1);
+                    }
+                }
+            },
+            Err(err) => {
+                println!("Error connecting to the socket. Error: {}. Retry {:?}", err, i);
+            }
+        }
+
+        thread::sleep(Duration::from_secs(6));
+    }
+
+    if !connected {
+        println!("Failed to write the message to the socket. Dropping the following message: {:?}", msg.payload_str());
     }
 }
